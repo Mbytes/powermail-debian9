9f90daeea78a5e6435515fcc2f55d652081ab661
Deepen Dhulla <deepen@deependhulla.com>
2017-07-21 14:48:25 +0530

    update

---
+++ letencrypt-ssl-setup-via-certbot.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+
+##certbot certonly -d `hostname` --standalone --agree-tos --email yourmail@example.com
+certbot certonly -d `hostname` --standalone --agree-tos 
+
+
+echo "MAILTO=\"\"" >> /var/spool/cron/crontabs/root 
+echo "30 2 * * 1 /usr/bin/certbot renew  >> /var/log/letsencrypt-renew.log" >> /var/spool/cron/crontabs/root 
+
+/etc/init.d/cron restart
+
+echo "manually update SSL in apache2,postfix,dovecot,haraka"
+echo "";
+
+
