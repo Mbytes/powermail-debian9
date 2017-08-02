9ef5cc858f71f620da647e023cf73b7e3c776bf3
Deepen Dhulla <deepen@deependhulla.com>
2017-08-02 12:00:09 +0530

    update

--- 06-add-admin-user-like-root-to-mysql.sh
+++ 06-add-admin-user-like-root-to-mysql.sh
@@ -3,7 +3,7 @@
 MYSQLPASSVPOP=`pwgen -c -1 8`
 echo $MYSQLPASSVPOP > /usr/local/src/mysql-admin-pass
 
-
+echo "mysql Admin password in /usr/local/src/mysql-admin-pass"
 
 
 echo "GRANT ALL PRIVILEGES ON *.* TO admin@localhost IDENTIFIED BY '$MYSQLPASSVPOP'" | mysql -uroot
