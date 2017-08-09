264c38e0641320d6fc06ce64134d2657b7b1f40e
Deepen Dhulla <deepen@deependhulla.com>
2017-08-09 13:50:16 +0530

    update

--- files/rootdir/var/www/html/powermailadmin/config.local.php
+++ files/rootdir/var/www/html/powermailadmin/config.local.php
@@ -1,8 +1,10 @@
 <?php
 $CONF['database_type'] = 'mysqli';
-$CONF['database_user'] = 'root';
-$CONF['database_password'] = 'techno02srv';
+$CONF['database_host'] = 'localhost';
+$CONF['database_user'] = 'powermail';
+$CONF['database_password'] = 'ohm8ahC2';
 $CONF['database_name'] = 'powermail';
+
 ## techno02srv
 $CONF['setup_password'] = 'b68f313c201e126212c4381cfad28fcc:25bdea750d5684fd3c2166b4d5ee80481c271bfb';
 $CONF['configured'] = true;
