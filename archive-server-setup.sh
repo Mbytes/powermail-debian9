a0af17ab051193795219b186cb9ef31def7a941d
Deepen Dhulla <deepen@deependhulla.com>
2017-08-09 16:36:41 +0530

    update

--- archive-server-setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# remove package if not on mail-server as archive does not need this.
-#apt-get remove spamassassin clamd  clamav-base  clamav-daemon  clamav-docs  clamav-freshclam  clamav-unofficial-sigs  clamdscan  redis-server  bind9 unbound
-#apt-get remove webmin
-#apt-get autoremove
-
-apt-get install recoll xapian-tools libpst4 antiword wv sqlite3-doc sqlite3
