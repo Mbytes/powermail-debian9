533716c91bc75802ab63c129ac9f2a6856b01388
Deepen Dhulla <deepen@deependhulla.com>
2018-01-01 12:40:16 +0530

    update

--- files/extra-addon/firefox-latest.sh
+++ files/extra-addon/firefox-latest.sh
@@ -4,12 +4,12 @@
 cd /tmp/
 
 mkdir /opt/firefox
-
+mv /opt/firefox/firefox /opt/firefox/firefox-old-version-backupon-`date +%s`
 wget -O FirefoxSetup.tar.bz2 "https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=en-US"
  tar xjf FirefoxSetup.tar.bz2 -C /opt/firefox/
 
-mv /usr/bin/firefox /usr/bin/firefox_orig
-mv /usr/lib/firefox-esr/firefox-esr /usr/lib/firefox-esr/firefox-esr_orig
+mv /usr/bin/firefox /usr/bin/firefox_orig-`date +%s`
+mv /usr/lib/firefox-esr/firefox-esr /usr/lib/firefox-esr/firefox-esr_orig-`date +%s`
 
 ln -s /opt/firefox/firefox/firefox /usr/lib/firefox-esr/firefox-esr
 ln -s /opt/firefox/firefox/firefox /usr/bin/firefox
